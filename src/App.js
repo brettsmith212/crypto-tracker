8af58574f978dde9a317c1a56c851cecde0d047b
Brett Smith <brettsmith212@gmail.com>
2021-11-07 15:29:08 -0800

    header finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import "./App.css";
+import { ThemeProvider } from "styled-components";
+import Navbar from "./components/Navbar";
+import GlobalStyles from "./components/styles/Global";
+
+const theme = {
+  colors: {
+    header: "#dee2e6",
+    body: "#fff",
+    footer: "#dee2e6",
+    font: "#4f4f4f",
+  },
+};
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Navbar></Navbar>
+      </>
+    </ThemeProvider>
+  );
 }
 
 export default App;
